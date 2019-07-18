d98a08bb0be341303960fe14e69fff43651f8f70
bettyChen <s9654025@gmail.com>
2019-07-18 22:33:57 +0800

    Init package

---
+++ app.js
@@ -0,0 +1,20 @@
+const express = require('express')
+const exphbs = require('express-handlebars')
+const bodyParser = require('body-parser')
+const methodOverride = require('method-override')
+
+const app = express()
+
+app.engine('handlebars', exphbs({ defaultLayout: 'main' }))
+app.set('view engine', 'handlebars')
+app.use(express.static('public'))
+app.use(bodyParser.urlencoded({ extended: true }))
+app.use(methodOverride('_method'))
+
+app.get('/', (req, res) => {
+  res.send('hello')
+})
+
+app.listen(3000, () => {
+  console.log(`Express is listening on http://localhost:3000`)
+})
