cae7880e02d77d91ad41e6e003917f4c3a5902af
bettyChen <s9654025@gmail.com>
2019-07-20 10:14:04 +0800

    add home and user routers

--- routes/users.js
+++ routes/users.js
@@ -1,5 +1,6 @@
 const express = require('express')
 const router = express.Router()
+const passport = require('passport')
 
 const db = require('../models')
 const Todo = db.Todo
@@ -18,12 +19,31 @@ router.get('/register', (req,res) => {
 })
 
 router.post('/register', (req,res) => {
-  User.create({
-    name: req.body.name,
-    email: req.body.email,
-    password: req.body.password
-  }).then(user => {
-    res.redirect('/')
+  const { name, email, password, password2 } = req.body
+
+  User.findOne({ where: {email: email} }).then( user => {
+    if (user) {
+      console.log('User already exist.')
+      res.render('register', {
+        name,
+        email,
+        password,
+        password2
+      })
+    } else {
+      const newUser = new User({
+        name,
+        email,
+        password
+      })
+      newUser
+        .save().then(user => {
+          res.redirect('/')
+        })
+        .catch(err => {
+          console.log(err)
+        })
+    }
   })
 })
 
